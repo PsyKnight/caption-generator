7cea8e0a677733780b1f315b190435b05c7f7acc
Ishanya Kukade <ishanyak12@gmail.com>
2025-04-22 22:04:26 +0530

    Complete getCaption

--- src/utils/gemini.ts
+++ src/utils/gemini.ts
@@ -1,9 +1,22 @@
-import {GoogleGenerativeAI} from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const ai = new GoogleGenerativeAI(import.meta.env.VITE_GEMNINI_API_KEY);
 
-const model = ai.getGenerativeModel({model: "gemini-1.5-flash"});
+const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const getGeminiResponse = (prompt) => {
+const getCaption = async ({
+  platform,
+  topic,
+  style,
+}: {
+  platform: string;
+  topic: string;
+  style: string;
+}) => {
+  const prompt = `Generate a caption for the platform '${platform}'. The topic is "${topic}". Write the caption in a ${style} way. Only write one caption. Do not write anything other than the caption. The caption should be not be too small.`;
+  const result = await model.generateContent(prompt);
+  const response = result.response;
+  return response.text();
+};
 
-}
+export default getCaption;
